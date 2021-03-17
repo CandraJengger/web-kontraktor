6b0be006c2a89a1d5494f016cdf39ddceb1850e2
CandraJengger <cakjengger@gmail.com>
2021-03-17 14:54:54 +0700

    Create MenuItem component

--- src/components/atoms/Logo/index.js
+++ src/components/atoms/Logo/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Image } from '@chakra-ui/react';
 
-const Logo = ({ path, alt }, props) => {
+const Logo = ({ path, alt, ...props }) => {
   return (
     <Box {...props}>
       <Image src={path} alt={alt} />
