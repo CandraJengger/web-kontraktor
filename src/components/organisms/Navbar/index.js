b5927d7ce4959afb07031bf028ccd94a7ce808e2
CandraJengger <cakjengger@gmail.com>
2021-03-17 15:12:03 +0700

    Create Navbar component

---
+++ src/components/organisms/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Logo from '../../atoms/Logo';
+import MenuToggle from '../../atoms/MenuToggle';
+import NavbarContainer from '../../atoms/NavbarContainer';
+import MenuLinks from '../../moleculs/MenuLinks';
+
+const Navbar = (props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <NavbarContainer {...props}>
+      <Logo w="100px" path="../../../assets/images/logo.svg" />
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <MenuLinks isOpen={isOpen} />
+    </NavbarContainer>
+  );
+};
+
+export default Navbar;
